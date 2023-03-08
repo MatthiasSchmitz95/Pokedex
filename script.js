f722871ebd1ab653f8dc014181827514fca77e02
Matthias Schmitz <m6schmitz123@gmail.com>
2023-03-08 12:27:21 +0100

    layout

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-
+let responseAsJson;
+let pokeName;
+let pokeImage;
 
 async function getInformation() {
     let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
     let response = await fetch(url);
-    let responseAsJson = await response.json();
-    let pokeName = responseAsJson['name'];
-    let pokeImage = responseAsJson['sprites']['front_default'];
+    responseAsJson= await response.json();
+    pokeName = responseAsJson['name'];
+    pokeImage = responseAsJson['sprites']['front_default'];
     console.log('Pokemon information', responseAsJson);
+    showPokemon();
+
+}
+
+function showPokemon(){
     document.getElementById('pokemonHeader').innerHTML = `${pokeName}
     `;
     document.getElementById('pokemonImage').src = pokeImage;
+
 }
