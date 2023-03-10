84453bbd8d5245107c5cda7bd50de1afd66904f1
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-03-10 12:30:11 +0100

    working functions

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let responseAsJson;
 let pokeName;
 let pokeImage;
-let pokemons = ["charmander", "squirtle", "bulbasaur","ditto"];
+let pokemons = ["charmander","charmeleon","charizard", "squirtle","	wartortle","blastoise", "bulbasaur","ivysaur","venusaur"];
+let currentPokemon;
+let stats;
+let pokemon;
 
 async function getInformation() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    let input = document.getElementById('search').value;
+    let url = `https://pokeapi.co/api/v2/pokemon/${input}`;
     let response = await fetch(url);
     responseAsJson = await response.json();
-    pokeName = responseAsJson['name'];
-    pokeImage = responseAsJson['sprites']['front_default'];
+    let currentPokemon = responseAsJson;
+    pokeName = currentPokemon['name'];
+    pokeImage = currentPokemon['sprites']['front_default'];
+    type = currentPokemon['types']['0']['type']['name'];
+    stats = currentPokemon['stats'];
+
     console.log('Pokemon information', responseAsJson);
     showPokemon();
 
@@ -16,14 +24,15 @@ async function getInformation() {
 
 async function renderFrontPage() {
     for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
+        let pokemon = pokemons[i];
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         let response = await fetch(url);
         responseAsJson = await response.json();
-        pokeName = responseAsJson['name'];
-        pokeImage = responseAsJson['sprites']['front_default'];
+        let currentPokemon = responseAsJson;
+        pokeName = currentPokemon['name'];
+        pokeImage = currentPokemon['sprites']['front_default'];
         document.getElementById('main-container').innerHTML += `
-        <div class="poke-box"><img class="pokemon" src="${pokeImage}" onclick="showCard(${i})">
+        <div class="poke-box"><img class="pokemon" src="${pokeImage}" onclick="showFrontPokemon(${i})">
         ${pokeName}
         </div>
         `;
@@ -32,19 +41,38 @@ async function renderFrontPage() {
 }
 
 function showCard(){
-    document.getElementById('main-container').classList.add('opacity');
     
+    document.getElementById('card-container').style.display ='';
+}
+
+function hideCard(){
+    document.getElementById('card-container').style.display ='none';
+    document.getElementById('search').innerHTML ='';
 }
 
 function showFrontPokemon(i){
-    document.getElementById('pokemonHeader').innerHTML = `${pokeName}
-    `;
+    document.getElementById('search').value = pokemons[i];
+    getInformation();
     
 }
 
 function showPokemon() {
+    document.getElementById('stats').innerHTML ='';
+    document.getElementById('type-container').innerHTML ='';
     document.getElementById('pokemonHeader').innerHTML = `${pokeName}
     `;
     document.getElementById('pokemonImage').src = pokeImage;
+for (let i = 0; i < stats.length; i++) {
+    let stat = stats[i]['base_stat'];
+    let statName = stats[i]['stat']['name'];
+    document.getElementById('stats').innerHTML+=`
+    <tr>
+        <td>${statName}</td>
+        <td>${stat}</td>
+    </tr>
+    `;
+}
+document.getElementById('type-container').innerHTML +=`Typ: ${type}`;
+showCard();
 
 }
