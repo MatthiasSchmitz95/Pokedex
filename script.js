e2ba145edeff62796b8b88a5974665015b4342e8
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-03-11 21:06:48 +0100

    showing pokemons still problem with showing every card

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let responseAsJson;
 let pokeName;
 let pokeImage;
-let pokemons = ["charmander","charmeleon","charizard", "squirtle","	wartortle","blastoise", "bulbasaur","ivysaur","venusaur"];
 let currentPokemon;
+let pokemons = [];
 let stats;
 let pokemon;
+let results;
+let pokeNrFrom = 1;
+let PokeNrTo = 100;
 
 async function getInformation() {
     let input = document.getElementById('search').value;
@@ -16,63 +19,94 @@ async function getInformation() {
     pokeImage = currentPokemon['sprites']['front_default'];
     type = currentPokemon['types']['0']['type']['name'];
     stats = currentPokemon['stats'];
-
     console.log('Pokemon information', responseAsJson);
     showPokemon();
 
 }
 
 async function renderFrontPage() {
-    for (let i = 0; i < pokemons.length; i++) {
-        let pokemon = pokemons[i];
-        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    //  let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+    //  let response = await fetch(url);
+    //  responseAsJson = await response.json();
+    //  let currentPokemon = responseAsJson['results'];
+   // console.log('informatio', currentPokemon);
+    for (let i = pokeNrFrom; i < PokeNrTo; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
         let response = await fetch(url);
         responseAsJson = await response.json();
         let currentPokemon = responseAsJson;
+        //console.log('information', currentPokemon);
         pokeName = currentPokemon['name'];
         pokeImage = currentPokemon['sprites']['front_default'];
+        pokemons.push(currentPokemon['name']);
         document.getElementById('main-container').innerHTML += `
-        <div class="poke-box"><img class="pokemon" src="${pokeImage}" onclick="showFrontPokemon(${i})">
+        <div class="poke-box"><img class="pokemon" src="${pokeImage}" onclick="showFrontPokemon(${i-1});window.scrollTo(0, 0)">
         ${pokeName}
         </div>
         `;
-
     }
+    console.log(pokemons);
 }
 
-function showCard(){
-    
-    document.getElementById('card-container').style.display ='';
+function renderSecondPage(startNr, endNr) {
+    pokemons = [];
+    document.getElementById('main-container').innerHTML = '';
+    pokeNrFrom = startNr;
+    PokeNrTo = endNr;
+    renderFrontPage();
+    console.log(pokemons);
 }
 
-function hideCard(){
-    document.getElementById('card-container').style.display ='none';
-    document.getElementById('search').value ='';
+function showCard() {
+    document.getElementById('card-container').style.display = '';
 }
 
-function showFrontPokemon(i){
+function hideCard() {
+    document.getElementById('card-container').style.display = 'none';
+    document.getElementById('search').value = '';
+}
+
+function showFrontPokemon(i) {
     document.getElementById('search').value = pokemons[i];
+    document.getElementById('main-container').innerHTML += `
+    <div id="card-container" style="display:none" onclick="hideCard()">
+        <div id="card">
+        <div id="pokemonHeader">
+        </div>
+        <div id="pokeInfo">
+            <img id="pokemonImage">
+            <div id="type-container">
+
+            </div>
+
+
+            <table id="stats">
+
+            </table>
+        </div>
+    </div>
+</div>`;
     getInformation();
-    
+
 }
 
 function showPokemon() {
-    document.getElementById('stats').innerHTML ='';
-    document.getElementById('type-container').innerHTML ='';
+    document.getElementById('stats').innerHTML = '';
+    document.getElementById('type-container').innerHTML = '';
     document.getElementById('pokemonHeader').innerHTML = `${pokeName}
     `;
     document.getElementById('pokemonImage').src = pokeImage;
-for (let i = 0; i < stats.length; i++) {
-    let stat = stats[i]['base_stat'];
-    let statName = stats[i]['stat']['name'];
-    document.getElementById('stats').innerHTML+=`
+    for (let i = 0; i < stats.length; i++) {
+        let stat = stats[i]['base_stat'];
+        let statName = stats[i]['stat']['name'];
+        document.getElementById('stats').innerHTML += `
     <tr>
         <td>${statName}</td>
         <td>${stat}</td>
     </tr>
     `;
-}
-document.getElementById('type-container').innerHTML +=`Typ: ${type}`;
-showCard();
+    }
+    document.getElementById('type-container').innerHTML += `Typ: ${type}`;
+    showCard();
 
 }
